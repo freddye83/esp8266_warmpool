498d60489ee79b0f1f55cd07ae9c4747b3e0bb73
freddye83 <39808136+freddye83@users.noreply.github.com>
2021-05-22 08:12:50 +0200

    Add files via upload

--- MQTT.ino
+++ MQTT.ino
@@ -6,23 +6,28 @@ void callback(char* p_topic, byte * p_payload, unsigned int p_length) {
     payload.concat((char)p_payload[i]);
   }
   String topic = String(p_topic);
+  debugW("Topic %s ", topic.c_str());
+  debugW("Payload %s ", payload.c_str());
 
-  mqtt.publish("POOLSTAR/debug", topic.c_str());
+  mqtt.publish("WARMPOOL/debug", topic.c_str());
   _yield();
 
   // handle message topic
-  if (topic.startsWith("POOLSTAR/command/")) {
+  if (topic.startsWith("WARMPOOL/command/")) {
     if (topic.endsWith("reset")) ESP.restart();
-  } else if (topic.startsWith("POOLSTAR/set/")) {
+  } else if (topic.startsWith("WARMPOOL/set/")) {
     if (topic.endsWith("target")) {
       m_target = payload.toInt();
+      m_mode = 0x01;
+      action = true;
       state.dirty = true;
     } else if (topic.endsWith("mode")) {
+      action = true;
       if (payload.equals("OFF")) m_mode = 0x00;
       else if (payload.equals("Heat")) m_mode = 0x84;
       else if (payload.equals("Cool")) m_mode = 0x81;
       else if (payload.equals("Auto")) m_mode = 0x88;
-      else if (payload.equals("ON")) m_mode = 0x88;
+      else if (payload.equals("ON")) m_mode = 0x01;
       state.sub = 0x02;
       state.dirty = true;
     }  else if (topic.endsWith("sub")) {
